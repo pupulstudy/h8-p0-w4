158fa7ae923c9937843905d0ec0654ef8e04f38a
pupulstudy <47462483+pupulstudy@users.noreply.github.com>
2019-07-20 21:41:28 +0700

    Create exercise-16.js

---
+++ exercise-16.js
@@ -0,0 +1,90 @@
+const graduates = students => {
+  let i = 0;
+  const result = new Object();
+
+  while (i < students.length) {
+    if (students[i]["score"] > 75) {
+      result[students[i]["class"]] === undefined
+        ? result[students[i]["class"]] = [{ name: students[i]["name"], score: students[i]["score"] }]
+        : result[students[i]["class"]].push({ name: students[i]["name"], score: students[i]["score"] });
+    }
+    i++;
+  }
+  return result;
+}
+
+/*console.log(graduates([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]))
+
+// {
+//   foxes: [
+//     { name: 'Dimitri', score: 90 }
+//   ],
+//   wolves: [
+//     { name: 'Alexei' , score: 85 },
+//     { name: 'Anastasia', score: 78 }
+//   ]
+// }
+
+console.log(graduates([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]))
+
+// {
+//   foxes: [
+//     { name: 'Alexander', score: 100 },
+//     { name: 'Vladimir', score: 92 }
+//   ],
+//   wolves: [
+//     { name: 'Alisa', score: 76 },
+//   ],
+//   tigers: [
+//     { name: 'Viktor', score: 80 }
+//   ]
+// }
+
+console.log(graduates([])) // {}*/
